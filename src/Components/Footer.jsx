ada9e484ffae34cbef4e6e0639bbd8e750eec3fa
FKNweeb <yosefkarbavi@gmail.com>
2024-11-28 11:56:11 +0100

    Updated footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,23 +1,35 @@
-import React from "react";
-import "./Footer.css";
+import React from 'react';
+import './Footer.css';
 
-function Footer(){
-    return (  
-        <div>
-        <div class="footerNavigation">
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">News</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Contact Us</a></li>
-                <li><a href="">our Team</a></li>
-            </ul>
-        </div>
-    <div class="footerBottom">
-        <p>Copyright &copy;2024; Designed by <span class="designer">groupe 3</span></p>
+function Footer() {
+  return (
+    <div>
+      <div class="footerNavigation">
+        <ul>
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">News</a>
+          </li>
+          <li>
+            <a href="">About</a>
+          </li>
+          <li>
+            <a href="">Contact Us</a>
+          </li>
+          <li>
+            <a href="">our Team</a>
+          </li>
+        </ul>
+      </div>
+      <div class="footerBottom">
+        <p>
+          Copyright &copy;2024; Designed by <span class="designer">Group 3</span>
+        </p>
+      </div>
     </div>
-    </div>
-    );
+  );
 }
 
 export default Footer;
