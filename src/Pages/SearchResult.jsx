d0d66c2815167dc41a9504bcb58226e6c2ed26be
DavorPavits <163551802+DavorPavits@users.noreply.github.com>
2024-12-05 18:31:25 +0100

    Search Result page Titles

--- src/Pages/SearchResult.jsx
+++ src/Pages/SearchResult.jsx
@@ -10,21 +10,35 @@ function SearchResult() {
     console.log(titles);
     console.log(names);
 
-    const [currentPage, setCurrentPage] = useState(1);
+    const [currentPageName, setCurrentPageName] = useState(1);
+    const [currentPageTitle, setCurrentPageTitle] = useState(1);
 
-    const handleNext = () =>{
-        setCurrentPage(currentPage => currentPage + 1)
+    const handleNextName = () =>{
+        setCurrentPageName(currentPage => currentPage + 1)
     }
 
-    const handlePrev = () => {
-        setCurrentPage( currentPage => currentPage -1)
+    const handlePrevName = () => {
+        setCurrentPageName( currentPage => currentPage -1)
     }
 
-    const indexLastItem = currentPage * 3;
+    const handleNextTitle = () =>{
+        setCurrentPageTitle(currentPage => currentPage + 1)
+    }
+
+    const handlePrevTitle = () => {
+        setCurrentPageTitle( currentPage => currentPage -1)
+    }
+
+    const indexLastItem = currentPageName * 3;
     const indexOfFirstItem = indexLastItem - 3;
-    const currItems = names.slice(indexOfFirstItem, indexLastItem);
+    const currNames = names.slice(indexOfFirstItem, indexLastItem);
 
-    const totalPages = Math.ceil(names.length / 3);
+    const indexLastItemTitle = currentPageTitle * 3;
+    const indexOfFirstItemTitle = indexLastItemTitle - 3;
+    const currTitles = titles.slice(indexOfFirstItemTitle, indexLastItemTitle);
+
+    const totalPagesName = Math.ceil(names.length / 3);
+    const totalPagesTitle = Math.ceil(titles.length / 3);
     return (
         <div className="container">
             <div className="row row-cols-2 gx-5">
@@ -33,14 +47,13 @@ function SearchResult() {
 
                     <h5 className="">People</h5>
                     <div id="Person" className="row justify-content-start mb-4">
-                        {console.log(currentPage)}
-                        {currItems.map( n => (
+                        {currNames.map( n => (
                         <div key={n.nconst} className="mb-1 col-12">
                             <div className="card w-100">
                                 <div className="card-body shadow-lg d-flex flex-column">
                                     <div className="d-flex flex-column flex-md-row justify-content-between">
-                                    <h5 className="card-title">{n.name}</h5>
-                                    <p className="fst-italic">{n.birthYear && <p>Birth year:{n.birthYear}</p> }</p>
+                                        <h5 className="card-title">{n.name}</h5>
+                                        <p className="fst-italic">{n.birthYear && <p>Birth year:{n.birthYear}</p> }</p>
                                     </div>
                                     <div className="d-flex flex-column justify-content-between mb-3">
                                     <div>
@@ -49,7 +62,7 @@ function SearchResult() {
                                     <div>
                                         {n.knownForTitles && n.knownForTitles.length > 0 && (
                                             <div>
-                                                Titles:{n.knownForTitles.map( t => (<span className="p-1 fst-bold">{t}</span>))}
+                                                Titles:{n.knownForTitles.map( t => (<span className="p-1 fst-bold">"{t}"</span>))}
                                             </div>
                                         )}
                                     </div>
@@ -61,18 +74,38 @@ function SearchResult() {
                     </div>
                         <div id="buttons" className="row d-flex">
                             <div className="col-12 d-flex flex-column flex-md-row justify-content-center  mb-3 p-2">
-                                <button className="btn btn-outline-primary w-25 mx-2 w-md-25 text-truncate" onClick={handlePrev} disabled={currentPage === 1}>Previous</button>
-                                <button className="btn btn-outline-primary w-25 mx-2 w-md-25 text-truncate" onClick={handleNext} disabled={currentPage === totalPages}>Next</button>
+                                <button className="btn btn-outline-primary w-25 mx-2 w-md-25 text-truncate" onClick={handlePrevName} disabled={currentPageName === 1}>Previous{console.log(currentPageTitle)}</button>
+                                <button className="btn btn-outline-primary w-25 mx-2 w-md-25 text-truncate" onClick={handleNextName} disabled={currentPageName === totalPagesName}>Next</button>
                             </div>
                         </div>
 
                     <h5 className="">Titles</h5>
                     <div id="Titles" className="row justify-content-start mb-4">
-                        {titles.slice(0, 3).map( t=> (
-                        <div key={t.tconst} className="mb-1">
+                        {currTitles.map( t=> (
+                        <div key={t.tconst} className="mb-1 col-12">
                             <div className="card w-100">
-                                <div className="card-body">
-                                    <h5>{t.primaryTitle}</h5>
+                                <div className="card-body shadow-lg d-flex flex-column">
+                                    <div className="d-flex flex-column flex-md-row justify-content-between">
+                                        <h5 className="card-title mb-0">{t.primaryTitle}</h5>
+                                        <p className="fst-italic">{t.startDate && <p>Aired: {t.startDate}</p>}</p>
+                                    </div>
+                                    <div className="mb-1">
+                                        {t.genres && t.genres.length > 0 && !t.genres.every(g => g===null) && (
+                                            <div>
+                                                Genre: {t.genres.map( g => (<span className="p-1 fst-bold">{g}</span>))}
+                                            </div>
+                                        )}
+                                    </div>
+                                    <div className="mb-1">
+                                        Type:{<span className="fst-italic p-1">{t.type}</span>}
+                                    </div>
+                                    <div className="mb-1">     
+                                        {t.languages && t.languages.length > 0 && !t.languages.every(l => l === null) && (
+                                            <div>
+                                                Lang: {t.languages.filter((e, i, self) => i === self.indexOf(e)).map( l => (<span className="p-1 fst bold">{l}</span>))}
+                                            </div>
+                                        )}
+                                    </div>
                                     <p className="text-long">{t.plot}</p>
                                 </div>
                             </div>
@@ -81,11 +114,10 @@ function SearchResult() {
                         )}
                     </div>
                         <div id="buttons" className="row d-flex">
-                            
-                                <div className="row justify-content-center mb-3 p-2">
-                                    <button className="w-25">Previous</button>
-                                    <button className="w-25">Next</button>
-                                </div>
+                            <div className="col-12 d-flex flex-column flex-md-row justify-content-center  mb-3 p-2">
+                                <button className="btn btn-outline-primary w-25 mx-2 w-md-25 text-truncate" onClick={handlePrevTitle} disabled={currentPageTitle === 1}>Previous</button>
+                                <button className="btn btn-outline-primary w-25 mx-2 w-md-25 text-truncate" onClick={handleNextTitle} disabled={currentPageTitle === totalPagesTitle}>Next</button> 
+                            </div>
 
                         </div>
                 </div>
